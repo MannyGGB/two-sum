1da8e599f4d7e371df2dfacac4025e84e6a37dd8
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-10-19 19:57:14 +0100

    Finalise

--- app.js
+++ app.js
@@ -21,9 +21,7 @@
 //   }
 // }
 
-const numberArray = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-];
+const numberArray = [1, 10, 3, 11, 14];
 
 let targetNumber = +prompt("Write a number between 1 and 20.");
 
