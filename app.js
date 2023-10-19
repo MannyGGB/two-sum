9cfed0a05413ba31bf2a49b5291d3892a96b06f1
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-10-19 19:44:39 +0100

    Finish logic

--- app.js
+++ app.js
@@ -2,27 +2,61 @@
 
 //   return [firstIndex,secondIndex]
 //   }
+
+// function twoSum(numberArray, targetNumber) {
+//   let firstIndex = 0;
+//   let secondIndex = numberArray.length - 1;
+//   while (firstIndex < secondIndex) {
+//     const sum = numberArray[firstIndex] + numberArray[secondIndex];
+//     if (sum === targetNumber) {
+//       prompt(
+//         `If you add ${firstIndex} and ${secondIndex}, you get ${targetNumber}`
+//       );
+//       return [firstIndex, secondIndex];
+//     } else if (sum < targetNumber) {
+//       firstIndex++;
+//     } else {
+//       secondIndex--;
+//     }
+//   }
+// }
+
 const numberArray = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 ];
 
-let targetNumber = prompt("Write a number between 1 and 20.");
+let targetNumber = +prompt("Write a number between 1 and 20.");
 
 function twoSum(numberArray, targetNumber) {
   let firstIndex = 0;
   let secondIndex = numberArray.length - 1;
-  while (firstIndex < secondIndex) {
-    const sum = numberArray[firstIndex] + numberArray[secondIndex];
-    if (sum === targetNumber) {
-      prompt(
-        `If you add ${firstIndex} and ${secondIndex}, you get ${targetNumber}`
-      );
-      return [firstIndex, secondIndex];
-    } else if (sum < targetNumber) {
+  let sum = numberArray[firstIndex] + numberArray[secondIndex];
+  //loop while the sum is not correct
+  while (sum !== targetNumber) {
+    //check if the first index plus the second index is less than the target number
+    if (sum < targetNumber) {
       firstIndex++;
-    } else {
+      if (firstIndex === numberArray.length - 1) {
+        break;
+      }
+      secondIndex = numberArray.length - 1;
+    } else if (sum > targetNumber) {
       secondIndex--;
+      if (firstIndex === secondIndex) {
+        break;
+      }
     }
+    sum = numberArray[firstIndex] + numberArray[secondIndex];
   }
+  if (sum === targetNumber) {
+    alert("Well done!");
+    alert(`${firstIndex} and ${secondIndex} are the indexes`);
+  } else {
+    alert("Not a valid answer.");
+  }
+  // if less, firstIndex++
+  //if more, secondIndex--
+  //if firstIndex is equal to the length of the array, then stop
 }
+
 twoSum(numberArray, targetNumber);
